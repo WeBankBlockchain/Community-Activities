e0b5cb0b117d62bf211e7d9b6af4cd65975f3c41
AOMEILSH <60667873+AOMEILSH@users.noreply.github.com>
2021-07-17 16:15:40 +0800

    Delete blockchain_courses/SCUT_2021/group4 program directory

--- blockchain_courses/SCUT_2021/group4 program/code/code/111/spring-boot-starter/spring-boot-starter-master/src/main/java/org/example/demo/constants/ContractConstants.java
+++
@@ -1,9 +0,0 @@
-package org.example.demo.constants;
-
-public class ContractConstants {
-    public static final String AccountAddress = "0x090c46f64195978efb1989b5d5c31039bd886591";
-
-    public static final String NTFMarketAddress = "0x33be6423ea38135de60cb3b7c862410596c7ca9b";
-
-    public static final String TransactionAddress = "0x5850dc1dd9da022a1fb65d94de96386d5992699b";
-}
